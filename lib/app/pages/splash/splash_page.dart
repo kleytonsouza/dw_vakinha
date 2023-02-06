f005e5a2e9d16fddf14b95e78a4f9083cb8fb040
kleytonsouza <kleytonl.souza@gmail.com>
2023-02-06 14:33:55 -0300

    second commit

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:dw_vakinha/app/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -5,9 +6,12 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Splash')),
-      body: const Text('fefef'),
+    return Theme(
+      data: ThemeData(scaffoldBackgroundColor: Colors.white),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Splash')),
+        body: const Text('fefef'),
+      ),
     );
   }
 }

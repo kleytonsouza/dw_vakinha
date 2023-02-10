fdbf5ca1398370a06778565a8623a6562f0c7914
kleytonsouza <kleytonl.souza@gmail.com>
2023-02-10 16:04:49 -0300

    add tela inicial

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,7 +1,6 @@
+import 'package:dw_vakinha/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw_vakinha/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
-
-import '../../core/config/env.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -9,24 +8,34 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            label: Env.i['backend_base_url'] ?? '',
-            onPressed: () {},
-            height: 200,
-            width: 200,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              label: Text('teste'),
+      body: ColoredBox(
+        color: const Color(0xFF140e0e),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  "assets/images/lanche.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-        ],
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentHeight(.20),
+                  ),
+                  Image.asset("assets/images/logo.png"),
+                  const SizedBox(height: 70),
+                  DeliveryButton(width: context.percentWidth(.60), height: 35, label: "Entrar", onPressed: (){})
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
